38a00e64037ba78f39bf075eb096aba2bc5a98ed
AlexandraBautistaChattin <balexandra96@gmail.com>
2022-07-23 16:17:48 -0700

    updated 2

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function newItem(url, left, bottom){
     })
 }
 
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -27,3 +28,4 @@ newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
